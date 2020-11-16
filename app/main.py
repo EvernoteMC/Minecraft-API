d6edad3f026c526df66fe4eb652b1d5ffa03787f
Darkflame72 <31436575+Darkflame72@users.noreply.github.com>
2020-11-16 21:37:14 +1300

    some more files

--- app/main.py
+++ app/main.py
@@ -1,15 +1,60 @@
-from typing import Optional
+import databases
+import sqlalchemy
+from fastapi import Depends, FastAPI, Header, HTTPException
 
-from fastapi import FastAPI
+from .routers import items, users
+from .tags import tags_metadata
 
-app = FastAPI()
+# SQLAlchemy specific code, as with any other app
+DATABASE_URL = "postgresql://user:password@postgresserver/db"
+
+database = databases.Database(DATABASE_URL)
+
+metadata = sqlalchemy.MetaData()
+
+notes = sqlalchemy.Table(
+    "notes",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("text", sqlalchemy.String),
+    sqlalchemy.Column("completed", sqlalchemy.Boolean),
+)
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+engine = sqlalchemy.create_engine(
+    DATABASE_URL, connect_args={"check_same_thread": False}
+)
+metadata.create_all(engine)
+
+app = FastAPI(
+    title="Obsidion-dev Minecraft API",
+    description="A Minecraft API brought to you by the folks at Obsidion-dev",
+    version="0.0.1",
+    openapi_tags=tags_metadata,
+    openapi_url="/api/v1/openapi.json",
+    redoc_url=None,
+)
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+async def get_token_header(x_token: str = Header(...)):
+    if x_token != "fake-super-secret-token":
+        raise HTTPException(status_code=400, detail="X-Token header invalid")
+
+
+app.include_router(users.router)
+app.include_router(
+    items.router,
+    prefix="/items",
+    tags=["items"],
+    dependencies=[Depends(get_token_header)],
+    responses={404: {"description": "Not found"}},
+)
+
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
---
+++ app/routers/__init__.py
@@ -0,0 +1,2 @@
+from app.routers import users
+from app.routers import items
